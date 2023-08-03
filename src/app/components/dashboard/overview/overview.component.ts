459e2b43d6464d931d5fbcfd97865142ec440f7e
cymarkSynergy <uchenna.ogbodo@simplifysynergy.com>
2023-08-03 09:32:41 +0100

    added terminal and user's form modals to Quick access

--- src/app/components/dashboard/overview/overview.component.ts
+++ src/app/components/dashboard/overview/overview.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-overview',
@@ -6,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent {
+  showModal = false;
+  showUserFormModal = false;
+  showTerminalFormModal = false;
+
+  myform : FormGroup;
+
+  constructor( private http: HttpClient,
+    private formBuilder: FormBuilder,
+    private router: Router){
+      this.myform = new FormGroup({
+        uName: new FormControl('',	[Validators.required]),
+        mobile: new FormControl('',	[Validators.required,  Validators.pattern(/^\+?\d{1,3}[-\s]?\d{9,11}$/)]),
+        password: new FormControl('',  [Validators.required]),
+        eMail: new FormControl('',  [ Validators.pattern('^.+@.+\..+$')]),
+        cOption: new FormControl('',   [Validators.required]),
+        optionChecked : new FormControl('',   [Validators.required]),
+   
+     }); 
+    }
+
+  get formData() { return this.myform.controls; };
+
+  toggleModal(){
+    this.showModal = !this.showModal;
+  }
+
+  toggleUserFormModal(){
+    this.showModal = !this.showModal;
+    this.showUserFormModal = !this.showUserFormModal;
+  }
+
+  toggleTerminalFormModal(){
+    this.showModal = !this.showModal;
+    this.showTerminalFormModal = !this.showTerminalFormModal;
+
+  }
+
+
 
 }
